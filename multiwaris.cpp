87eaeb78b5c3feccc19ca34c5d1cd01e930220e7
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-18 08:27:51 +0700

    pelajar mewarisi orang secara virtual(hanya satu salinan)

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -12,3 +12,13 @@ public:
              << endl;
     }
 };
+
+class pelajar : virtual public orang{
+public:
+    pelajar(int pUmur) : 
+    orang(pUmur)
+    {
+        cout << "pelajar di buat\n"
+             << endl;
+    }
+};
