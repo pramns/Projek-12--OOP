249b0ac963434bcf9e5709b3a7487700f92a0445
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-18 07:41:12 +0700

    create class with void setsaklarno

--- Enkasulapsi.cpp
+++ Enkasulapsi.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+class remotelampu{
+  private:
+    string saklarno[5];
+  public:
+    void setsaklarno(int i,string value){
+      saklarno[i] = value;
+    }
+    
+}
