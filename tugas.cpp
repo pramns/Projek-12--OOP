c027081fc82200a692a0d6d1b123b553e1dd4e6f
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-18 10:11:42 +0700

    membuat rekeningsyariah mewarisi rekening bank

--- tugas.cpp
+++ tugas.cpp
@@ -6,3 +6,23 @@ class RekeningBank {
     virtual void potongAdmin() = 0;
     virtual ~RekeningBank() {}
 };
+
+
+class RekeningSyariah : public RekeningBank {
+  private:
+    string nama;
+    double saldo;
+
+  public:
+    void setRekening(string n, double s){
+      nama = n;
+      saldo = s;
+    }
+    void potongAdmin() override {
+      cout << "Nama     : " << nama << endl;
+      cout << "Tipe     : Syariah" << endl;
+      cout << "Admin    : Bebas biaya admin" << endl;
+      cout << "Saldo    : Rp " << saldo << endl;
+      cout << "----------------------------" << endl;
+    }
+};
