f915b7d0221dc4afa0e409e4549fceff61661939
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-18 08:00:44 +0700

    create class derived class

--- NoOverriding.cpp
+++ NoOverriding.cpp
@@ -7,3 +7,11 @@ class baseclass{
       cout << "Haloo saya Function dari base class";
     }
 };
+
+class derivedclass : public baseclass{
+  public: 
+    void perkenalan1(){
+      cout << "Hallo saya function dari devired class";
+    }
+};
+
