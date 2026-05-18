ecaddaf1e35d51e44faf4c1b3c7e5634b0bff384
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-18 10:19:00 +0700

    membuat main function

--- tugas.cpp
+++ tugas.cpp
@@ -46,3 +46,51 @@ class RekeningKonvensional : public RekeningBank {
       cout << "----------------------------" << endl;
     }
 };
+
+class RekeningPremium : public RekeningBank {
+  private:
+    string nama;
+    double saldo;
+
+  public:
+    void setRekening(string n, double s){
+      nama = n;
+      saldo = s;
+    }
+    void potongAdmin() override {
+      cout << "Nama     : " << nama << endl;
+      cout << "Tipe     : Premium" << endl;
+      if(saldo > 10000000){
+        cout << "Admin    : Bebas biaya admin" << endl;
+      } else {
+        saldo -= 50000;
+        cout << "Admin    : Rp 50.000 dipotong" << endl;
+      }
+      cout << "Saldo    : Rp " << saldo << endl;
+      cout << "----------------------------" << endl;
+    }
+};
+
+int main(){
+  RekeningSyariah rs;
+  rs.setRekening("Sono", 5000000);
+
+  RekeningKonvensional rk;
+  rk.setRekening("Nur", 2000000);
+
+  RekeningPremium rp1;
+  rp1.setRekening("Yono", 15000000);
+
+  RekeningPremium rp2;
+  rp2.setRekening("Helmi", 8000000);
+
+  cout << "=== Proses Akhir Bulan - Bank Gibran Jaya ===" << endl;
+  cout << "----------------------------" << endl;
+
+  rs.potongAdmin();
+  rk.potongAdmin();
+  rp1.potongAdmin();
+  rp2.potongAdmin();
+
+  return 0;
+}
